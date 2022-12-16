ce5b615cb9f5cade1dcfe5e8586e2a1449d0a08b
Semen Samosiuk <samasiuk1986@gmail.com>
2022-12-16 11:47:27 +0300

    feat:add library react-router-dom

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-
+import { Routes, Route } from "react-router-dom";
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
