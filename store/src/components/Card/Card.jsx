89f5c19badfa5aac03e2170074a9e9234bcffb71
AlexKaroh <steam.alexkaroh@gmail.com>
2022-12-19 21:56:44 +0400

    feat: add display of pictures from API

--- store/src/components/Card/Card.jsx
+++ store/src/components/Card/Card.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import style from "./cadr.module.css";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
+import { CoverageMap } from "istanbul-lib-coverage";
 
 function Card({item}) {
   console.log(item.images);
   return (
     <Link to="card">
-      <div className={style.cadr__cards}>
+      <div style={{background:`url(${item.images[1]})`,backgroundSize: `cover`}} className={style.cadr__cards}>
         <div className ={classNames(style.card_name, style.border__bottom__card, style.font)}>{item.title}</div>
         <div className ={style.card_two_rows}>
           <div className ={classNames(style.card_add, style.border__bottom__card, style.font)}>Add</div>
