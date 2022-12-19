53bbc18b673802faa1a23e4a646093c13deb2df5
Semen Samosiuk <samasiuk1986@gmail.com>
2022-12-19 15:24:15 +0300

    feat: add redux

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -5,8 +5,22 @@ import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import BasketPage from './pages/BasketPage/BasketPage';
 import Card from './pages/Card/Card';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch('https://63a042fa24d74f9fe832fb1e.mockapi.io/items')
+    .then(res => {return res.json()})
+    .then(data => {
+      setProduct(data);
+    })
+  },[])
+
+
+
   return (
     <div className='wrapper'>
       <Header/>
--- store/src/components/SortFirst/SortFirst.jsx
+++ store/src/components/SortFirst/SortFirst.jsx
@@ -5,7 +5,6 @@ import style from './sortFirst.module.css';
 
 
 function SortFirst({title}) {
-  console.log(title);
   return (
     <div className={style.category_check}>
       <input type="checkbox" /><span className={style.font}>{title}</span>
