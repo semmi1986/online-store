61f89368118807d79cab16f0a042214706a153d5
Semen Samosiuk <samasiuk1986@gmail.com>
2022-12-21 16:39:51 +0300

    fix: sort category

--- store/src/components/BottomCover/bottomCover.tsx
+++ store/src/components/BottomCover/bottomCover.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import style from './bottomCover.module.css';
+import React from "react";
+import style from "./bottomCover.module.css";
 import classNames from "classnames";
 
 function BottomCover() {
-    return (
-<div className={classNames(style.bottom__cover)}><span className={style.font}> </span></div>
-    )};
+  return (
+    <div className={classNames(style.bottom__cover)}>
+      <span className={style.font}> </span>
+    </div>
+  );
+}
 
 export default BottomCover;
---
+++ store/src/components/SortBrands/SortBrands.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import style from './sortBrands.module.css';
+
+
+function SortBrands({brands, onClickCategoryBrands}) {
+  const [activeIndex, setActiveIndex] = useState()
+
+
+  function handelClick(value, index) {
+    onClickCategoryBrands(value);
+    setActiveIndex(index)
+  }
+
+  return(
+    <>
+    {brands.map((item, i) =>(
+      <div key={i} className={style.category_check}>
+        <ul>
+          <li  className={activeIndex == i ? style.active : ''} onClick= {() => handelClick(item, i)}>{item}</li>
+        </ul>
+      </div>
+    ))}
+    </>
+  )
+}
+
+export default SortBrands
