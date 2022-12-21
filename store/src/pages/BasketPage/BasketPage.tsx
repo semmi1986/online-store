0f0d0a2708877a30ae34bdc24d7e719dbf530ac2
AlexKaroh <steam.alexkaroh@gmail.com>
2022-12-21 18:29:13 +0400

    feat: add style for basket page

--- store/src/pages/BasketPage/BasketPage.tsx
+++ store/src/pages/BasketPage/BasketPage.tsx
@@ -1,9 +1,35 @@
 import { Link } from "react-router-dom";
+import style from "./BasketPage.module.css";
+import classNames from "classnames";
 
 function BasketPage() {
   return (
+    <div className={style.basket__page_container}>
+      <div className={style.basket__page_two_rows}>
+        <div className={style.products__container}>
 
-    <div>This is BASKETS</div>
+        </div>
+        <div className={style.summary__container}>
+          <div className={style.summary__container_header}>
+            <span className={style.font}>Summary</span>
+          </div>
+          <div className={style.summary__content}>
+          <div>
+            <span className={style.font}>
+              Proucts : 
+              <span className={style.font2}> 0</span>
+            </span>
+          </div>
+          <div>
+            <span className={style.font}>
+              Total € : 
+              <span className={style.font2}> 0</span>
+            </span>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
