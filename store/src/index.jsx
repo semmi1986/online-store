e65e80bfcc4b58517aea9e7855f3bf98d4e6c02a
AlexKaroh <steam.alexkaroh@gmail.com>
2022-12-27 12:54:09 +0400

    fix:

--- store/src/index.jsx
+++ store/src/index.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root")
 );
